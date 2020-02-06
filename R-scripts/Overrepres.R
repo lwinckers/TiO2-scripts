bb8ee324bc5fb381eec57ea3e94e5ff5f185e772
Laurent2207 <laurentwinckers@gmail.com>
2020-02-06 16:07:11 +0100

    Update

--- R-scripts/Overrepres.R
+++ R-scripts/Overrepres.R
@@ -15,31 +15,34 @@ library(plyr)
 # load annotated GO terms
 path = paste0(getwd(), "/data-output")
 ls_GO <- list.files(path = path, pattern = "^ann_GO")
-for (i in 1:length(ls_GO)){assign(ls_GO[i], read.table(paste0(path,"/",ls_GO[i]), header = T, sep = "\t"))}
+for (i in 1:length(ls_GO)){
+  assign(ls_GO[i], read.table(paste0(path,"/",ls_GO[i]), header = T, sep = "\t"))
+  }
 ls_GO <- mget(ls(pattern = "ann_GO"))
-
 ls_GO <- lapply(ls_GO, function(x){as.numeric(x$entrezgene_id)})
 
-# load gmt files and combine them
+# load gmt files and transpose the obtained lists to data frames
 path = paste0(getwd(), "/data-input")
 gmtFile <- list.files(path = path, pattern = ".gmt")
-for (i in 1:length(gmtFile)){assign(gmtFile[i], read.gmt(paste0(path,"/",gmtFile[i])))}
+for (i in 1:length(gmtFile)){
+  assign(gmtFile[i], read.gmt(paste0(path,"/",gmtFile[i])))
+  }
 
-#gmtFile <- mget(ls(pattern = ".gmt"))
-#list2env(lapply(gmtFile, function(x){ldply(x,data.frame)}), envir = .GlobalEnv)
+gmtFile <- mget(ls(pattern = ".gmt"))
+list2env(lapply(gmtFile, function(x){plyr::ldply(x,data.frame)}), envir = .GlobalEnv)
 
+# combine pathway database files and clean it
 databases <- do.call(rbind, lapply(ls(pattern = ".gmt"), get))
 colnames(databases) <- c("pathway", "entrezgene")
-
 databases$entrezgene <- as.numeric(databases$entrezgene)
 
 # enricher
 for (i in 1:length(ls_GO)){
-res <- as.data.frame(enricher(gene = ls_GO[[i]], minGSSize = 1, TERM2GENE = databases, pvalueCutoff = 0.05))
-res$GOterm <- names(ls_GO[i])
-assign(paste0("result_", names(ls_GO[i])), as.data.frame(res))
-write.table(as.data.frame(res), paste0("./data-output/results_", names(ls_GO[i])), quote = F, sep = "\t", row.names = F)
-}
+  res <- as.data.frame(enricher(gene = ls_GO[[i]], minGSSize = 10, TERM2GENE = databases, pvalueCutoff = 0.01))
+  res$GOterm <- names(ls_GO[i])
+  assign(paste0("result_", names(ls_GO[i])), as.data.frame(res))
+  write.table(as.data.frame(res), paste0("./data-output/results_", names(ls_GO[i])), quote = F, sep = "\t", row.names = F)
+  }
 
 # save files
 results <- rbind(result_ann_GO0006915.txt, result_ann_GO0006954.txt,
