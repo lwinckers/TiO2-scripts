3a6e9a1916f645d926632ba5831b86ecc3ac0903
LaurentW <laurent_2207@live.nl>
2020-04-02 17:21:49 +0200

    Update GSEA.R

--- functions/GSEA.R
+++ functions/GSEA.R
@@ -2,6 +2,8 @@
 ### Laurent Winckers, Maastricht University - Department of Bioinformatics BiGCaT
 ### 2020-03-23
 
+library(enrichplot)
+
 GSEAanalysis <- function(GENESET, fileName, data){
   for(i in 4:9) {
     dat <- as.numeric(data[,i])
@@ -9,9 +11,10 @@ GSEAanalysis <- function(GENESET, fileName, data){
     dat <- sort(dat, decreasing = T)
     # run GSEA
     res <- GSEA(dat, pvalueCutoff = 1,
-                minGSSize = 10, maxGSSize = 500,
+                minGSSize = 1, maxGSSize = 5000,
                 pAdjustMethod = "BH", TERM2GENE = GENESET,
                 nPerm = 1000)
+    #p1 <<- gseaplot2(res, geneSetID = 1:3, pvalue_table = TRUE)
     loc = paste(getwd(), paste0("/output/GSEA/GSEA_",fileName,"-",i-3,".txt"),sep="")
     write.table(res, file = loc, sep="\t", quote = F,
                 row.names = F)
