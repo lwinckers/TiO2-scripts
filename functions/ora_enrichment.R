c9e2be8fa635f8b3a368fe696d9eb601ef5618e9
Laurent Winckers <laurent_2207@live.nl>
2021-07-24 13:30:04 +0200

    Refactor functions
    Set the specific path to output folder outside of functions for better reproducibility

--- functions/ora_enrichment.R
+++ functions/ora_enrichment.R
@@ -2,7 +2,7 @@
 ### Laurent Winckers, Maastricht University - Department of Bioinformatics BiGCaT
 ### 2021-06-28
 
-ora_enrichment <- function(genes, wpid2gene, wpid2name, prefix) {
+ora_enrichment <- function(genes, wpid2gene, wpid2name, prefix, path) {
   res <- clusterProfiler::enricher(
     genes,
     pvalueCutoff = 1,
@@ -11,5 +11,5 @@ ora_enrichment <- function(genes, wpid2gene, wpid2name, prefix) {
     TERM2GENE = wpid2gene,
     TERM2NAME = wpid2name)
   
-  write.table(res, paste0("output/",prefix,".txt"), quote = F, sep = "\t", row.names = F)
+  write.table(res, paste0(path, prefix, ".txt"), quote = F, sep = "\t", row.names = F)
 }
